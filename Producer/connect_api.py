c05305c60544c5a256173f1203a74f8cd9d3bba1
Jie Hou <namehb@gmail.com>
2025-02-13 12:18:12 +0100

    create connect1. conenct_api.py, coin_producer_ETH.py

---
+++ Producer/connect_api.py
@@ -0,0 +1,24 @@
+from constants import COINMARKET_API
+from requests import Session, Timeout, TooManyRedirects
+import requests  
+import json
+
+API_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+
+def get_latest_coin_data(target_symbol="BTC"):
+
+    parameters = {"symbol": target_symbol, "convert": "USD"}
+    headers = {
+        "Accepts": "application/json",       
+        "X-CMC_PRO_API_KEY": COINMARKET_API,}
+
+    session = Session()
+    session.headers.update(headers)    
+
+    try:
+        response = session.get(API_URL, params=parameters)  
+        response.raise_for_status() 
+        return response.json().get("data", {}).get(target_symbol, None)               
+    except (ConnectionError, Timeout, TooManyRedirects, json.JSONDecodeError) as e:
+        print(f"API request failed:{e}")
+        return None
