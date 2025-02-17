b5e8c75811588169f12cc8c2fc137495867a2742
Marcus Ericsson <marcus.ericsson_92@icloud.com>
2025-02-17 13:58:02 +0100

    dashboard streamlit with graf

--- Streamlit dash/dashboard.py
+++ Streamlit dash/dashboard.py
@@ -7,6 +7,7 @@ from streamlit_autorefresh import st_autorefresh
 from sqlalchemy import create_engine
 import pandas as pd
 from constants.constants import (POSTGRES_DBNAME, POSTGRES_HOST, POSTGRES_PASSWORD, POSTGRES_PORT, POSTGRES_USER)
+from charts import line_chart
 
 currency = ["SEK", "NOK", "DKK", "EUR", "USD"]
 connection_string = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DBNAME}"
@@ -19,13 +20,23 @@ def load_data(query):
         df = df.set_index("timestamp")
         return df
     
+        
+    
+    
 def main():
-    # df = load_data("SELECT * FROM cardano;")
+    df = load_data("SELECT * FROM cardano;")
 
     st.markdown("# Streamed data for Cardano(ADA) from coinmarket")
+    
 
     st.selectbox("Choose currency to show", currency)
     if currency == "SEK":
+       pass
+   
+   
+    st.markdown("## Latest price in USD for Cardano")
+    price_chart = line_chart(x= df.index, y= df["price_usd"], title= "price USD")
+    st.pyplot(price_chart)
 
 
 if __name__ == "__main__":
